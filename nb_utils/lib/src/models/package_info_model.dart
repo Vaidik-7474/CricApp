4d7aac582423a93b1570fe3d180d4888a91ee605
mjagad1196 <mihirjagad@gmail.com>
2022-10-17 12:45:11 +0530

    -> Issue fixed of duration variable.

---
+++ nb_utils/lib/src/models/package_info_model.dart
@@ -0,0 +1,15 @@
+class PackageInfoData {
+  String? appName;
+  String? versionName;
+  String? packageName;
+  String? versionCode;
+  int? androidSDKVersion;
+
+  PackageInfoData({
+    this.appName,
+    this.versionName,
+    this.packageName,
+    this.versionCode,
+    this.androidSDKVersion,
+  });
+}
